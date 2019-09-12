110db7800493ecd659c4e14c2e5d4b9d713acc26
Kyle Mulqueen <kmulqdrums710@gmail.com>
2019-09-11 21:24:54 -0600

    Fixed re-direct when clicking the title.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Nav.css";
 const Nav = props => (
-    <nav>
-        <ul>
-            <li className="brand">
-                <a href="/clicky-game/">{props.title}</a>
-            </li>
-            <li id="correct-incorrect">{props.correctIncorrect}</li>
-            <li className="alignRight">Score - Top: {props.topScore} | Current: {props.score}</li>
-        </ul>
-    </nav>
+  <nav>
+    <ul>
+      <li className="brand">
+        <a href="/Clicky-Game/">{props.title}</a>
+      </li>
+      <li id="correct-incorrect">{props.correctIncorrect}</li>
+      <li className="alignRight">
+        Score - Top: {props.topScore} | Current: {props.score}
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Nav;
